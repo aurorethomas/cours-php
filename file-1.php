0b62e97cdc284619bf53ab1a404e27ec86cc4f26
aurore <aurorethomas4@gmail.com>
2020-04-06 12:12:18 +0200

    mon premier cours php

---
+++ file-1.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(E_ALL);
+
+$prenom1 = 'Quentin';
+$prenom2 = 'Christophe';
+$prenom3 = 'Aurore';
+$espace = ' ';
+
+echo('Merci pour ce cours ' . $prenom1 . $espace . $prenom2 . $espace . $prenom3);
