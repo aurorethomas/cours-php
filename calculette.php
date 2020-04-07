6476bc64cdcbdff6b2aa2457be335aa376eac0f8
aurore <aurorethomas4@gmail.com>
2020-04-07 15:09:19 +0200

    calculette qui ne permet pas de diviser par 0

--- calculette.php
+++ calculette.php
@@ -1,17 +1,32 @@
 <?php
 // ma calculette
+
 $resultat = 0;
+
 //var_dump($argc);
 //var_dump($argv);
-switch($argv[2])
-{
+
+
+switch($argv[2]){
     case '+':
         $resultat = $argv[1] + $argv[3];
+
     case '-':
         $resultat = $argv[1] - $argv[3];
+
     case '*':
         $resultat = $argv[1] * $argv[3];
+
     case '/':
-        $resultat = $argv[1] / $argv[3];
+        if ($argv[3] == 0)
+        {
+            $resultat='Diviser par zéro, en voilà une drôle d\'idée';
+        }
+        else
+        {
+            $resultat = $argv[1] / $argv[3];
+        } 
+
 }   
+
 echo($resultat);
